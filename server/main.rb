51af21128d39ebae9b00f784c1a14816c0880040
saj7 <scott2000jones@gmail.com>
2022-03-05 14:11:45 +0000

    database structure, drafted new endpoints

--- server/main.rb
+++ server/main.rb
@@ -2,9 +2,14 @@ require 'sinatra'
 require 'sequel'
 
 DB = Sequel.sqlite('database.db')
-# DB.run "CREATE TABLE users (name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)"
+# DB.run "CREATE TABLE users (name VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY, password VARCHAR(255) NOT NULL)"
+# DB.run "CREATE TABLE teams (team_name VARCHAR(255) NOT NULL UNIQUE PRIMARY KEY)"
+# DB.run "CREATE TABLE team_user_lookup (team_name VARCHAR(255) NOT NULL UNIQUE, user_name VARCHAR(255) NOT NULL UNIQUE, FOREIGN KEY (team_name) REFERENCES teams (team_name), FOREIGN KEY (user_name) REFERENCES users (name))"
+# DB.run "CREATE TABLE dms (team_name VARCHAR(255) NOT NULL, sent_by VARCHAR(255) NOT NULL, content VARCHAR(255), timestamp INTEGER, FOREIGN KEY (team_name) REFERENCES teams (team_name), FOREIGN KEY (sent_by) REFERENCES users (name))"
 
 puts DB.schema(:users)
+puts DB.schema(:teams)
+puts DB.schema(:team_user_lookup)
 get '/' do
     DB[:users].all.to_s
 end
@@ -34,4 +39,15 @@ post "/login" do
     @name = params["name"]
     @password = params["password"]
     check_in_db(@name, @password) ? "TRUE" : "FALSE"
+end
+
+post "/create_team" do
+    @team_name = params["team_name"]
+    "NOT IMPLEMENTED YET"
+end
+
+post "/add_user_to_team" do
+    @team_name = params["team_name"]
+    @user_name = params["user_name"]
+    "NOT IMPLEMENTED YET"
 end
