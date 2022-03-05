17dbc31dfd96a7e36a36310d042def79ecc87212
KatyCluckie <krcluckie@gmail.com>
2022-03-05 15:20:41 +0000

    basic contact page

--- gui/src/Components/Contacts.jsx
+++ gui/src/Components/Contacts.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 
+
+
 class Contacts extends Component {
-    state = {  } 
-    render() { 
-        return (
-        <Card>
-            <Card.Body>
-                
-            </Card.Body>
-        </Card>);
+    state = {
+
+        contacts: [["Hackathon", "Join for Fun"], ["CS Ball", "Matrix Theme, join for info"], ["CS4099", "Dissertation"]]
+    }
+    render() {
+
+        return this.state.contacts.map(data => {
+            return (
+                <div>
+                    <Card  style={{ width: '18rem' }} className='mt-4 border-danger' shadow={4}>
+                        <Card.Body>
+                            <Card.Title> {data[0]}</Card.Title>
+                            <Card.Text>
+                                {data[1]}
+                            </Card.Text>
+                            <Card.Link href="#">Add People to Team</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </div>
+            )
+        })
+
     }
 }
- 
+
 export default Contacts;
