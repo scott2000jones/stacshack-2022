b16368323bf6e011bfb40ff2a0f72949ea5c14ce
saj7 <scott2000jones@gmail.com>
2022-03-05 11:48:10 +0000

    login endpoint working

--- server/main.rb
+++ server/main.rb
@@ -3,18 +3,35 @@ require 'sequel'
 
 DB = Sequel.sqlite('database.db')
 # DB.run "CREATE TABLE users (name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)"
+
 puts DB.schema(:users)
 get '/' do
-    # DB.run "INSERT INTO users (name, password) VALUES (\"HELLO\",\"TEST\")"
     DB[:users].all.to_s
 end
 
 post "/register" do
     @name = params["name"]
     @password = params["password"]
-    puts @name 
-    puts @password
-    puts "INSERT INTO users (name, password) VALUES (\"" + @name + "\", \"" + @password + "\")"
     DB.run "INSERT INTO users (name, password) VALUES (\"" + @name + "\", \"" + @password + "\")"
     "Ok!"
+end
+
+get "/drop_all_users" do
+    DB.run "DELETE FROM users"
+    "Ok!"
+end
+
+def check_in_db(target_name, target_pw)
+    DB[:users].all.each do |item|
+        if item[:name] == target_name && item[:password] == target_pw then
+            return true
+        end
+    end
+    return false
+end
+
+post "/login" do
+    @name = params["name"]
+    @password = params["password"]
+    check_in_db(@name, @password) ? "TRUE" : "FALSE"
 end
