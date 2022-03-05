aec6654bc74c793ef55badf4d8089dd5b3e132db
RAMcCracken <rhona.mccracken@btinternet.com>
2022-03-05 12:04:45 +0000

    Router added to react app, bootstrap added

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Welcome from "./Components/Welcome";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacc
-        </a>
-      </header>
+    <Router>
+    <div className="App h-100">
+  
+        {/* <Route component={HeaderBar}/> */}
+        <div className="container-md">
+          <Switch>
+            <Route exact path="/" component={Welcome}/>
+            <Route path="/home" component={Welcome}/>
+          </Switch>
+        </div>
     </div>
+    </Router>
   );
 }
 
