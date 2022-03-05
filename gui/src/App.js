462b536a9547fc1f92b4bd3a376b2689bd174dfb
RAMcCracken <rhona.mccracken@btinternet.com>
2022-03-05 15:16:30 +0000

    navbar added

--- gui/src/App.js
+++ gui/src/App.js
@@ -4,12 +4,14 @@ import Welcome from "./Components/Welcome";
 import Login from "./Components/Login";
 import Contacts from "./Components/Contacts";
 import Call from "./Components/Call";
+import Navigation from './Components/Navigation';
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
  
 function App() {
   return (
     <Router>
+      <Route component={Navigation}/>
     <div className="App h-100">
   
         {/* <Route component={HeaderBar}/> */}
