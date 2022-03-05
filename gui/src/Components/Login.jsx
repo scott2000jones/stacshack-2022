53c7224e04409297eb45da4b9f844af159f920b1
ik40 <irakoshy@gmail.com>
2022-03-05 15:14:44 +0000

    API REQUESTS

--- gui/src/Components/Login.jsx
+++ gui/src/Components/Login.jsx
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import {login as apiLogin} from "../lib/api"
 
 class Login extends Component {
-    state = {  } 
+
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            password: "",
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.login = this.login.bind(this);
+    }
+
+    handleChange = key => event => {
+        this.setState({
+            [key]: event.target.value
+        })
+    }
+
+    login(e) {
+        e.preventDefault()
+        apiLogin(this.state.name, this.state.password)
+    }
+
     render() { 
         return (
             <form className='mt-5 border-secondary'>
                 <h3>Sign In</h3>
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" placeholder="Enter email" value={this.state.name} onChange={this.handleChange("name")} />
                 </div>
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" placeholder="Enter password" value={this.state.password} onChange={this.handleChange("password")} />
                 </div>
                 <div className="form-group">
                     <div className="custom-control custom-checkbox">
@@ -21,7 +43,7 @@ class Login extends Component {
                         <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                     </div>
                 </div>
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="submit" className="btn btn-primary btn-block" onClick={this.login}>Submit</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
